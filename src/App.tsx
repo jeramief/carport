1f4e3bcc5fe03a093627280602091a4c54fd8c92
jeramief <109633173+jeramief@users.noreply.github.com>
2023-07-01 01:09:18 -0400

    building home, about, store

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
+import { Container } from 'react-bootstrap'
+import { Route } from 'react-router-dom'
+import { About } from './pages/About'
+import { Home } from './pages/Home'
+import { Store } from './pages/Store'
 
 function App() {
 
   return (
-    <>
-      <h1>hi</h1>
-    </>
+    <Container>
+      <Route path='/' element={<Home />} />
+      <Route path='/store' element={<Store />} />
+      <Route path='/about' element={<About />} />
+    </Container>
   )
 }
 
