24bcaaf3b7676f7624e69c4cee66e122bd84699c
jeramief <109633173+jeramief@users.noreply.github.com>
2023-07-02 17:04:57 -0400

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
-import { Route } from 'react-router-dom'
 import { About } from './pages/About'
 import { Home } from './pages/Home'
 import { Store } from './pages/Store'
 
+
 function App() {
 
   return (
-    <Container>
-      <Route path='/' element={<Home />} />
-      <Route path='/store' element={<Store />} />
-      <Route path='/about' element={<About />} />
-    </Container>
+      <Container className='mb-4'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/store' element={<Store />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Container>
   )
 }
 
