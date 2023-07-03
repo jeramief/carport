ae757c727bea5b27af01ac0fc2b9f397ddefd5f9
jeramief <109633173+jeramief@users.noreply.github.com>
2023-07-03 19:45:12 -0400

    added shopping cart button

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import { Routes, Route } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import { About } from './pages/About'
-import { Home } from './pages/Home'
-import { Store } from './pages/Store'
-
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Navbar } from "./components/Navbar";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
 
 function App() {
-
   return (
-      <Container className='mb-4'>
+    <>
+      <Navbar />
+      <Container className="mb-4">
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/store' element={<Store />} />
-          <Route path='/about' element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Container>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
